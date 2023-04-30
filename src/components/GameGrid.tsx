c1e26b1cd30605757e6a9ea0ae888714ca2399a0
Matan Shaviro <matan3sh@gmail.com>
2023-04-30 20:24:00 +0300

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,7 +15,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         spacing={10}
-        padding={"10px"}
+        padding={3}
       >
         {loading &&
           skeletons.map((skeleton) => (
