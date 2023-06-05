0a0391647d69784207b6189dd67602ddf1d079ee
Matan Shaviro <matan3sh@gmail.com>
2023-06-05 20:38:39 +0300

    Getting the platforms from object

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,12 @@
 import useData from "./useData"
-
+import platforms from "../data/platforms"
 interface Platform {
   id: number
   name: string
   slug: string
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents")
+const usePlatforms = () => ({ data: platforms, loading: false, error: null })
+// const usePlatforms = () => useData<Platform>("/platforms/lists/parents")
 
 export default usePlatforms
